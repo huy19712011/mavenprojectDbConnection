1b7e40ca673902e318ae27b3340607d3ac0b143f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-21 16:57:43 +0700

    adding one more connection pool for testing

---
+++ src/main/java/JdbcConnectionPool2/Main.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JdbcConnectionPool2;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author huynq
+ * works properly!!! 20-7-2021
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Connection Pool");
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = DatabaseConnectionPoolManager.getConnection();
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM employees";
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("first_name"));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+
+            if (resultSet != null) try {
+                resultSet.close();
+            } catch (SQLException ignore) {
+            }
+            if (statement != null) try {
+                statement.close();
+            } catch (SQLException ignore) {
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException ignore) {
+            }
+
+        }
+    }
+
+}
